07fb8f16e313933be45ac990eb2e2ef0547ddbf9
radoslaw.jeruzal <radoslaw.jeruzal@xcaliber.com>
2020-03-23 14:33:07 +0100

    slack format introduce

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
+import logging
 from fastapi import FastAPI
-from .schemas.msg import Msg
+from .schemas.slack import Command
+from starlette.requests import Request
+
+log = logging.getLogger(__name__)
 app = FastAPI(debug=True, redoc_url=None)
+
 
 @app.get("/")
 async def hello_world():
     return "hello"
 
 @app.post("/task/")
-async def add_task(msg: Msg):
-    print(msg)
-    return msg
+async def add_task(request: Request):
+    form = await request.form()
+    command = Command(**form)
+    log.info(command)
+    return command
---
+++ app/schemas/slack.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class Command(BaseModel):
+    token: str
+    command: str
+    response_url: str
+    trigger_id: str
+    user_id: str
+    user_name: str
+    team_id: str
+    channel_id: str
+    text: str
