3465638ae7add2b5e660b45c8f99dfbcdbbfff7c
radoslaw.jeruzal <radoslaw.jeruzal@xcaliber.com>
2020-03-23 22:02:47 +0100

    proper base for crud

--- app/models/item.py
+++ app/models/item.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, Integer, String
 
-from app.db.base_class import CustomBase
+from app.db.base_class import Base
 
 
-class Item(CustomBase):
+class Item(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     priority = Column(Integer, index=True)
