71aa40619ea5e8891f0ecd519ee038c3351ddf5f
radoslaw.jeruzal <radoslaw.jeruzal@xcaliber.com>
2020-03-23 14:15:30 +0100

    route change

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-
+from .schemas.msg import Msg
 app = FastAPI(debug=True, redoc_url=None)
 
 @app.get("/")
 async def hello_world():
     return "hello"
 
-@app.post("/task")
-async def add_task(msg: str):
+@app.post("/task/")
+async def add_task(msg: Msg):
     print(msg)
     return msg
