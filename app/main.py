868da9c4e6aaade0a9f37987d0292d4cd6345fe5
radoslaw.jeruzal <radoslaw.jeruzal@xcaliber.com>
2020-03-23 14:40:23 +0100

    logs to stdout

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 import logging
+import sys
+
 from fastapi import FastAPI
 from .schemas.slack import Command
 from starlette.requests import Request
 
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 log = logging.getLogger(__name__)
 app = FastAPI(debug=True, redoc_url=None)
 
@@ -16,4 +19,5 @@ async def add_task(request: Request):
     form = await request.form()
     command = Command(**form)
     log.info(command)
+
     return command
