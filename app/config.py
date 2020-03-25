a4cc6608e4a6f6482547ea542c258fc30275b163
radoslaw.jeruzal <radoslaw.jeruzal@xcaliber.com>
2020-03-25 15:55:50 +0100

    docker playground

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
 import os
 
-DATABASE_URL = os.environ.get('DATABASE_URL') or "postgresql://postgres:postgres@"+os.environ.get('DB_HOST')+":5432/postgres"
+DATABASE_URL = os.environ.get('DATABASE_URL') or \
+               f"postgresql://{os.environ['DB_USER']}:{os.environ['DB_PW']}@{os.environ['DB_HOST']}:5432/{os.environ['DB_NAME']}"
--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,1 +1,22 @@
 import pytest
+from sqlalchemy.exc import ProgrammingError
+
+from app.db.init_db import init_db
+from app.db.session import db_session, engine
+from app.db.base_class import Base
+from app.utils import log
+
+@pytest.fixture(autouse=True, scope="module")
+def database_service():
+    init_db(db_session, engine)
+
+@pytest.fixture(autouse=True)
+def cleanup_service(database_service):
+    yield
+    tables = reversed(Base.metadata.sorted_tables)
+
+    for table in tables:
+        try:
+            db_session.execute(f"truncate table {table.name} cascade;")
+        except ProgrammingError:
+            log.warning("table does not exist", table=table)
