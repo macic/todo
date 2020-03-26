b34a2a1d00e5f2372caef39969859efe5a97273d
radoslaw.jeruzal <radoslaw.jeruzal@xcaliber.com>
2020-03-26 14:16:20 +0100

    CRUD basic tests

--- app/tests/crud/test_item.py
+++ app/tests/crud/test_item.py
@@ -16,49 +16,49 @@ def test_create_item():
     assert item.priority == priority
     assert item.user_id == user_id
 
+def test_get_item():
+    title = random_lower_string()
+    priority = 123
+    item_in = ItemCreate(title=title, priority=priority)
+    user_id = 'C@234'
+    item = crud.item.create_with_owner(
+        db_session=db_session, obj_in=item_in, user_id=user_id
+    )
+    stored_item = crud.item.get(db_session=db_session, id=item.id)
+    assert item.id == stored_item.id
+    assert item.title == stored_item.title
+    assert item.priority == stored_item.priority
+    assert item.user_id == stored_item.user_id
 #
-# def test_get_item():
-#     title = random_lower_string()
-#     description = random_lower_string()
-#     item_in = ItemCreate(title=title, description=description)
-#     user = create_random_user()
-#     item = crud.item.create_with_owner(
-#         db_session=db_session, obj_in=item_in, owner_id=user.id
-#     )
-#     stored_item = crud.item.get(db_session=db_session, id=item.id)
-#     assert item.id == stored_item.id
-#     assert item.title == stored_item.title
-#     assert item.description == stored_item.description
-#     assert item.owner_id == stored_item.owner_id
-#
-#
-# def test_update_item():
-#     title = random_lower_string()
-#     description = random_lower_string()
-#     item_in = ItemCreate(title=title, description=description)
-#     user = create_random_user()
-#     item = crud.item.create_with_owner(
-#         db_session=db_session, obj_in=item_in, owner_id=user.id
-#     )
-#     description2 = random_lower_string()
-#     item_update = ItemUpdate(description=description2)
-#     item2 = crud.item.update(db_session=db_session, db_obj=item, obj_in=item_update)
-#     assert item.id == item2.id
-#     assert item.title == item2.title
-#     assert item2.description == description2
-#     assert item.owner_id == item2.owner_id
+def test_update_item():
+    title = random_lower_string()
+    priority = 123
+    item_in = ItemCreate(title=title, priority=priority)
+    user_id = 'C@234'
+    item = crud.item.create_with_owner(
+        db_session=db_session, obj_in=item_in, user_id=user_id
+    )
+    title2 = random_lower_string()
+    item_update = ItemUpdate(title=title2)
+    item2 = crud.item.update(db_session=db_session, db_obj=item, obj_in=item_update)
+    assert item.id == item2.id
+    assert item.title == item2.title
+    assert item2.title == title2
+    assert item.user_id == item2.user_id
 #
 #
-# def test_delete_item():
-#     title = random_lower_string()
-#     description = random_lower_string()
-#     item_in = ItemCreate(title=title, description=description)
-#     user = create_random_user()
-#     item = crud.item.create_with_owner(db_session=db_session, obj_in=item_in, owner_id=user.id)
-#     item2 = crud.item.remove(db_session=db_session, id=item.id)
-#     item3 = crud.item.get(db_session=db_session, id=item.id)
-#     assert item3 is None
-#     assert item2.id == item.id
-#     assert item2.title == title
-#     assert item2.description == description
-#     assert item2.owner_id == user.id
+def test_delete_item():
+    title = random_lower_string()
+    priority = 123
+    item_in = ItemCreate(title=title, priority=priority)
+    user_id = 'C@234'
+    item = crud.item.create_with_owner(
+        db_session=db_session, obj_in=item_in, user_id=user_id
+    )
+    item2 = crud.item.remove(db_session=db_session, id=item.id)
+    item3 = crud.item.get(db_session=db_session, id=item.id)
+    assert item3 is None
+    assert item2.id == item.id
+    assert item2.title == title
+    assert item2.title == title
+    assert item2.user_id == user_id
